2ea56763bd549987bdbeec87460cd1f2b3960e1c
Sahil Rajwar <sahilrajwar2004@gmail.com>
2024-05-26 19:10:20 +0530

    v-0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "matrix",
-    version = "0.1.1",
+    version = "0.1.2",
     author = "Sahil Rajwar",
     description = "python library for doing calculations with matrices",
     long_description = open("README.md").read(),
@@ -10,6 +10,7 @@ setup(
     license = "MIT",
     url = "https://github.com/Sahil-Rajwar-2004/matrix.py",
     packages = find_packages(),
+    requires = ["numpy"],
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
