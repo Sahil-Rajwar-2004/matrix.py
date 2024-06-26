28042f1081511b81f655c0b99274b21820f0f982
Sahil Rajwar <sahilrajwar2004@gmail.com>
2024-06-27 00:27:13 +0530

    installation file

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+pkg="matrix"
+
+pip show "$pkg" > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "$pkg lib is already installed"
+    exit 0
+else
+    echo "$pkg lib isn't installed yet"
+fi
+
+echo "Initiating the installation process..."
+
+if [ -e "./setup.py" ]; then
+    echo "./setup.py has been found!"
+else
+    echo "setup.py file not found!"
+    exit 1
+fi
+
+set -e
+
+python3 ./setup.py sdist bdist_wheel
+cd ./dist
+pip install *.whl
+cd ..
+rm -rf dist build matrix.egg-info
+
+pip show "$pkg" > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "$pkg lib has been installed successfully"
+else
+    echo "something bad has happened! try to install manually"
+    exit 1
+fi
