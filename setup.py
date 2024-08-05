9f80dfe5fc7a5920e6880c9a6fbcdc456f36483f
Sahil Rajwar <sahilrajwar2004@gmail.com>
2024-08-05 17:08:40 +0530

    v-0.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 
 setup(
     name = "matrix",
-    version = "0.5.0",
+    version = "0.5.1",
     author = "Sahil Rajwar",
     description = "python library for doing calculations with matrices",
     long_description = open("README.md","r",encoding = "utf-8").read(),
